db4da420875313dc7a3eb26278b1873375c2d84a
Ash <ashenglandelbro@protonmail.com>
2020-02-25 17:07:22 +0000

    Setup Post component

--- src/page/WhatsOn.js
+++ src/page/WhatsOn.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import MainPage from "./MainPage";
 import $ from 'jquery';
+import Card from "react-bootstrap/Card";
 
 // Fetches posts asyncthorusly from the backend
 function getPosts(user, num, successCallback, errorCallback) {
@@ -21,13 +22,33 @@ class Post extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            likes: 0,
+            dislikes: 0,
+        }
+    }
+
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Subtitle>{this.props.author}</Card.Subtitle>
+                </Card.Body>
+                <Card.Text>
+                    {this.props.desc}
+                </Card.Text>
+            </Card>
+        );
     }
 
 
 }
 
 Post.propTypes = {
-
+    title: React.PropTypes.string.isRequired,
+    desc: React.PropTypes.string.isRequired,
+    author: React.PropTypes.string.isRequired,
 };
 
 class WhatsOn extends MainPage {
@@ -60,5 +81,5 @@ class WhatsOn extends MainPage {
 
 
 WhatsOn.propTypes = {
-    user: React.PropTypes.object.isRequired()
+    user: React.PropTypes.object.isRequired
 };
